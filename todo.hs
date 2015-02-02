ab3fc67b8c90cbc15bc694a1e8849e177d61a991
Vinay Bhat <vinaybhat001@gmail.com>
2015-02-02 17:30:11 +0530

    made data directory

--- todo.hs
+++ todo.hs
@@ -1,1 +1,7 @@
--- Todo.hs is a powerful tool to integrate and 
+-- Todo.hs is a powerful tool to log, and sync mobile with web.
+-- Sync Folder -> Logic Code -> Sync Folder -> Phone -> GNote -> 
+-- Sync Folder
+
+
+-- Assume existence of three files which are as follows: 
+-- toweek.txt, totoday.txt, tolog.txt
