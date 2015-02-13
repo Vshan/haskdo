ab3423362504f9ade2bacadbad11666c09926dc8
Vinay Bhat <vinaybhat001@gmail.com>
2015-02-13 21:20:59 +0530

    More todo.hs

--- todo.hs
+++ todo.hs
@@ -5,3 +5,38 @@
 
 -- Assume existence of three files which are as follows: 
 -- toweek.txt, totoday.txt, tolog.txt
+
+{--
+ - Here's how tolog.txt would look like: 
+ 0730 got up
+ 0740 crap
+ 0750 leave for class
+ 0800 class begin
+ 0850 break
+ 0900 next class
+ .
+ .
+ .
+ 0200 sleep
+--}
+
+
+{--
+ - Here's how totoday.txt would look like: 
+ -
+ -
+ -
+ -
+ -
+--}
+
+
+{-- Here's how toweek.txt would look like: 
+ -
+ -
+ -
+ -
+ -
+ -
+--}
+
