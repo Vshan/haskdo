8e6fa98e03d3dfdaa0d06b8df3a77ac86e886595
Vinay Bhat <vinaybhat001@gmail.com>
2015-02-25 13:14:13 +0530

    Fleshing out ruby script

--- conversion.rb
+++ conversion.rb
@@ -4,7 +4,10 @@ File.open('temp.rb', 'w') do |f|
   file_to_array.each_with_index do |l, i|
     cur_line = l
     next_line = file_to_array[i+1]
-    new_line
+    start_time = cur_line.partition(" ").first
+    fin_time = next_line.partition(" ").first
+    desc = cur_line.partition(" ").last
+    new_line = start_time + " " + fin_time + " " + desc
     f.puts new_line
   end
 end
