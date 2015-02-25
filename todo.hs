9aa115f782f0c88932c50d7cf5762cf2fb123cf9
Vinay Bhat <vinaybhat001@gmail.com>
2015-02-25 18:33:18 +0530

    Fleshing out todo.hs

--- todo.hs
+++ todo.hs
@@ -64,7 +64,7 @@ import Data.Time
 type Etime = (Int, Int)
 
 data Etype = Study | Code | Read | Write | Hygiene | Exercise | Class
-             | ClassWork | Food | Zone | Sleep deriving Show
+             | ClassWork | Food | Zone | Sleep deriving (Show, Eq)
 
 data Event = Event { desc :: String,
                      etimeBegin :: Etime,
@@ -109,5 +109,16 @@ getEtype x = case (take 2 $ reverse x) of
                   "f:" -> Food
                   "z:" -> Zone
                   "y:" -> Sleep
+
+getTimeSpentinMin :: Etime -> Etime -> Int
+getTimeSpentinMin (x1, y1) (x2, y2) = (x2 - x1)*60 + (y2 - y1)
+
+isOfType :: Event -> Etype -> Bool
+x `isOfType` t = (etype x == t)
+
+timeSpentOn :: Etype -> [Event] -> Int
+timeSpentOn x es = sum
+                   $ map (\v -> getTimeSpentinMin (etimeBegin v) (etimeEnd v))
+                   $ filter (\b - > x `isOfType` b) es
 
 gento :: [a] -> [b] -> String
